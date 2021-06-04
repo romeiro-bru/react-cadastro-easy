1b784c54c3720bc409e70ab939bae4919da4cb38
Bruna Romeiro <56081906+romeiro-bru@users.noreply.github.com>
2021-06-04 10:44:15 -0300

    Add userRegister path

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./styles.css";
 import { Header } from "./components/Header/Header";
 import { Home } from "./pages/Home/home";
+import { UserRegister } from "./pages/UserRegister/userRegister";
 
 export default function App() {
   return (
     <Router>
-      <div className="App">
-        <Header />
-        <Route exact path="/" component={Home} />
-      </div>
+      <Switch>
+        <div className="App">
+          <Header />
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/userRegister">
+            <UserRegister />
+          </Route>
+        </div>
+      </Switch>
     </Router>
   );
 }
