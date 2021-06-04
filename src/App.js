604641b7391c2bfc60890d651f15d0f695b31399
Bruna Romeiro <56081906+romeiro-bru@users.noreply.github.com>
2021-06-03 23:51:10 -0300

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./styles.css";
 import { Header } from "./components/Header/Header";
 import { Home } from "./pages/Home/home";
 
 export default function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route exact path="/" component={Home} />
+      </div>
+    </Router>
   );
 }
