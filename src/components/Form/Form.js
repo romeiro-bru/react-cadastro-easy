71311b2d2b1c5ce0e227d826efb4d2e810476b35
Bruna Romeiro <56081906+romeiro-bru@users.noreply.github.com>
2021-06-24 17:07:09 -0300

    Remove section classname form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -10,7 +10,7 @@ const url = "https://private-21e8de-rafaellucio.apiary-mock.com/users";
 export function Form() {
   const [inputs, setInputs] = useState({});
   const [response, setResponse] = useState([]);
-  const [hideSpin, setHideSpin] = useState(true);
+  const [hiddenSpin, setHiddenSpin] = useState(true);
   const history = useHistory();
 
   useEffect(() => {
@@ -36,7 +36,7 @@ export function Form() {
     e.preventDefault();
     setResponse([...response, inputs]);
     setTimeout(() => {
-      setHideSpin(false);
+      setHiddenSpin(false);
     }, 1000);
     setTimeout(() => {
       history.push("/userregister");
@@ -46,96 +46,95 @@ export function Form() {
   return (
     <>
       <Header />
-      <section className="form">
-        <form onSubmit={handleSubmit}>
-          <div className="floating">
-            <input
-              id="fullname"
-              className="floating-input"
-              value={response.name}
-              name="name"
-              onChange={handleInputChange}
-              placeholder="Nome completo"
-              required
-            ></input>
-            <label
-              htmlFor="inputId"
-              className="floating-label fullname-label"
-              data-content="Nome Completo (sem abreviações)"
-            >
-              <span className="hidden-visually">Nome Completo</span>
-            </label>
-          </div>
 
-          <div className="floating">
-            <input
-              className="floating-input"
-              value={response.email}
-              name="email"
-              onChange={handleInputChange}
-              placeholder="E-mail"
-              required
-            ></input>
-            <label
-              htmlFor="inputId"
-              className="floating-label email-label"
-              data-content="E-mail"
-            >
-              <span className="hidden-visually">E-mail</span>
-            </label>
-          </div>
-
-          <div className="floating">
-            <input
-              className="floating-input"
-              value={response.cpf}
-              name="cpf"
-              onChange={handleInputChange}
-              placeholder="CPF"
-              required
-            ></input>
-            <label
-              htmlFor="inputId"
-              className="floating-label cpf-label"
-              data-content="CPF"
-            >
-              <span className="hidden-visually">CPF</span>
-            </label>
-          </div>
-
-          <div className="floating">
-            <input
-              className="floating-input"
-              value={response.phone}
-              name="phone"
-              onChange={handleInputChange}
-              placeholder="Telefone"
-              maxLength="11"
-              required
-            ></input>
-            <label
-              htmlFor="inputId"
-              className="floating-label phone-label"
-              data-content="Telefone"
-            >
-              <span className="hidden-visually">Telefone</span>
-            </label>
-          </div>
-
-          <button
-            disabled={response.length > 3}
-            hidden={hideSpin === false}
-            className="button-register"
-            type="submit"
+      <form onSubmit={handleSubmit}>
+        <div className="floating">
+          <input
+            id="fullname"
+            className="floating-input"
+            value={response.name}
+            name="name"
+            onChange={handleInputChange}
+            placeholder="Nome completo"
+            required
+          ></input>
+          <label
+            htmlFor="inputId"
+            className="floating-label fullname-label"
+            data-content="Nome Completo (sem abreviações)"
           >
-            Cadastrar
-          </button>
-          <button hidden={hideSpin} className="button-register load">
-            {" "}
-            <AiOutlineLoading className="spin" />
-          </button>
-        </form>
-      </section>
+            <span className="hidden-visually">Nome Completo</span>
+          </label>
+        </div>
+
+        <div className="floating">
+          <input
+            className="floating-input"
+            value={response.email}
+            name="email"
+            onChange={handleInputChange}
+            placeholder="E-mail"
+            required
+          ></input>
+          <label
+            htmlFor="inputId"
+            className="floating-label email-label"
+            data-content="E-mail"
+          >
+            <span className="hidden-visually">E-mail</span>
+          </label>
+        </div>
+
+        <div className="floating">
+          <input
+            className="floating-input"
+            value={response.cpf}
+            name="cpf"
+            onChange={handleInputChange}
+            placeholder="CPF"
+            required
+          ></input>
+          <label
+            htmlFor="inputId"
+            className="floating-label cpf-label"
+            data-content="CPF"
+          >
+            <span className="hidden-visually">CPF</span>
+          </label>
+        </div>
+
+        <div className="floating">
+          <input
+            className="floating-input"
+            value={response.phone}
+            name="phone"
+            onChange={handleInputChange}
+            placeholder="Telefone"
+            maxLength="11"
+            required
+          ></input>
+          <label
+            htmlFor="inputId"
+            className="floating-label phone-label"
+            data-content="Telefone"
+          >
+            <span className="hidden-visually">Telefone</span>
+          </label>
+        </div>
+
+        <button
+          disabled={response.length > 3}
+          hidden={hiddenSpin === false}
+          className="button-register"
+          type="submit"
+        >
+          Cadastrar
+        </button>
+        <button hidden={hiddenSpin} className="button-register load">
+          {" "}
+          <AiOutlineLoading className="spin" />
+        </button>
+      </form>
     </>
   );
 }
