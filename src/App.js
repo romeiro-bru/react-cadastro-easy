2b30cbbb1a4cd2a0a67114b03de8b082118978e3
Bruna Romeiro <56081906+romeiro-bru@users.noreply.github.com>
2021-06-25 00:12:01 -0300

    Change styles to componets folder, Move assets to public

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./assets/styles/main.scss";
+import "./styles.scss";
 import { Form } from "./components/Form/Form";
 import { User } from "./components/User/User";
 
