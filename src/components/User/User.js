2de8bfba9e2e727cb6ab7f9eff9563d321732de0
Bruna Romeiro <56081906+romeiro-bru@users.noreply.github.com>
2021-06-29 11:41:03 -0300

    Add fnc delete item from localStorage

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -11,9 +11,14 @@ export function User() {
     ls !== null ? setData(ls) : setData("");
   }, []);
 
-  const handleRemove = () => {
-    localStorage.removeItem("storeData");
-    history.push("/");
+  const handleRemove = (itemIndex) => {
+    const items = JSON.parse(localStorage.getItem("storeData"));
+    const filtered = items.filter((_, index) => index !== itemIndex);
+    localStorage.setItem("storeData", JSON.stringify(filtered));
+
+    setTimeout(() => {
+      history.push("/");
+    }, 1000);
   };
 
   const handleReturn = () => {
@@ -33,7 +38,10 @@ export function User() {
               <p>{item.cpf}</p>
               <p>{item.phone}</p>
 
-              <button onClick={handleRemove} className="button-delete">
+              <button
+                onClick={() => handleRemove(index)}
+                className="button-delete"
+              >
                 Excluir cadastro
               </button>
             </li>
